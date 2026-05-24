d6af819eeca59c3bb41e14847bf9b03eaf154f44
nayangoria <77434516+nayangoria@users.noreply.github.com>
2026-05-25 02:11:50 +0530

    update axiosInstance for production added env file

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,9 +2,8 @@ import axios from "axios"
 
 // Create a custom axios instance
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
 })
-
 // This runs before every REQUEST — adds token automatically
 axiosInstance.interceptors.request.use(
     (config) => {
