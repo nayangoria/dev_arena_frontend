90c63ece89e25266fac12eb5ef419bcf13167f76
nayangoria <77434516+nayangoria@users.noreply.github.com>
2026-05-27 08:49:17 +0530

    updated codeEditor

--- frontend/src/Pages/CodeEditor.jsx
+++ frontend/src/Pages/CodeEditor.jsx
@@ -17,8 +17,8 @@ const starterCode = {
 #     print(x)
 
 `,
-    java: `import java.util.Scanner;
-public class Solution {
+java: `import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // Write your solution here
