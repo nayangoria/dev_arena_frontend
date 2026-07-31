29be07a7b46d7a6065b7dc6681f4d9097e8cbd6f
nayan <nayangoria1234@example.com>
2026-07-31 16:26:14 +0530

    bug fixed in profile page

--- frontend/src/Pages/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage.jsx
@@ -6,7 +6,7 @@ import { BattleCard } from "../Component/BattleCard";
 import { useNavigate } from "react-router-dom";
 import StatCard from "../Component/StatCard";
 
-const AI_JUDGE_API = "http://localhost:8081";
+const AI_JUDGE_API = import.meta.env.VITE_AI_JUDGE_URL;
 
 export default function ProfilePage() {
   const [rooms, setRooms] = useState([]);
