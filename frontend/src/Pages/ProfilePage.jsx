5ad3c05cb6e10065d3c37c773c800b7183450854
nayan <nayangoria1234@example.com>
2026-08-01 21:59:49 +0530

    fixed api call in profilepage

--- frontend/src/Pages/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage.jsx
@@ -6,7 +6,7 @@ import { BattleCard } from "../Component/BattleCard";
 import { useNavigate } from "react-router-dom";
 import Statcard from "../Component/Statcard";
 
-const AI_JUDGE_API = import.meta.env.VITE_AI_JUDGE_URL || "http://localhost:8081";
+const AI_JUDGE_API = import.meta.env.AI_JUDGE_API || "http://localhost:8081";
 
 export default function ProfilePage() {
   const [rooms, setRooms] = useState([]);
